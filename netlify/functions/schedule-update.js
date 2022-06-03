13b94ad39228deedd6489a588275294689edc4a1
Brendan Sheehan <15129375+feemcgill@users.noreply.github.com>
2022-06-02 17:04:45 -0700

    update

--- netlify/functions/schedule-update.js
+++ netlify/functions/schedule-update.js
@@ -1,16 +1,19 @@
 const fetch = require('node-fetch')
 
-const { schedule } = require('@netlify/functions')
-
-const handler = async function (event, context) {
-  console.log('UPDATE SCHED')
-  let call = await fetch('https://squiggle-functions.netlify.app/.netlify/functions/update-squigs-background')
-  console.log('CALLED UPDATE SQUIGS:', call)
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(call),
+exports.handler = async (event, context) => {
+  try {
+    console.log('')
+    console.log('‹‹›⁄⁄⁄⁄⁄⁄⁄⁄›‹›‹‹‹‹‹››‹‹‹‹‹››››‹‹‹‹‹‹‹‹‹‹››‹›‹‹››››')
+    console.log('')
+    console.log('UPDATE SCHED :::::')
+    let call = await fetch('https://squiggle-functions.netlify.app/.netlify/functions/update-squigs-background')
+    console.log('CALLED UPDATE SQUIGS:', call)
+    return { statusCode: 200, body: JSON.stringify({ call }) }
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    }
   }
 }
-
-module.exports.handler = schedule('@hourly', handler)
